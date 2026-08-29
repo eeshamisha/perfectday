cb2b4e06a783e70e58e78ce3c579517454866214
katherineqi123 <katherine.qi@education.nsw.gov.au>
2026-08-29 06:24:11 +0000

    h

--- java.js
+++ java.js
@@ -1,16 +1,26 @@
-const scores = {
+const scores = JSON.parse(localStorage.getItem('quizscore')) || {
   rainy: 0,
   cold: 0,
   sunny: 0,
 };
 
 const buttons = document.querySelectorAll('.btn');
-const nextButton = document.getElementById('nextButton');
-const backButton = document.getElementById('backButton');
+const reset = document.getElementById('resetButton');
 
-nextButton.addEventListener('click', function() {
+function saveScore() {
   const input = document.querySelector('input:checked');
   const choice = input.value; 
   scores[choice] += 1;
+  localStorage.setItem('quizscore', JSON.stringify(scores));
   console.log(scores);
-})
+}
+
+reset.addEventListener('click', () => {
+  localStorage.clear();
+}
+
+const results = document.getElementById('resultspage');
+if (results){
+  console.log(scores);
+  console.log(Math.max(...scores));
+}
