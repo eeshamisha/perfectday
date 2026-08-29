18ea46d6ae3079561af44fd44766a1506afeab7b
eeshamisha <ij91010@gmail.com>
2026-08-29 07:03:27 +0000

    4

--- java.js
+++ java.js
@@ -18,6 +18,9 @@ function saveScore() {
 const results = document.getElementById('resultspage');
 const here = document.getElementById('here');
 const rBtn = document.getElementById("resultsBtn");
+rBtn.addEventListener("click",function(){
+  
+})
 if (results){
   const highestPlayer = scores.reduce((max, item) => item > max.item ? item : max);
   console.log(highestPlayer);
