6ba48cea8ea0dae2048a917a51240ba28d67da89
katherineqi123 <katherine.qi@education.nsw.gov.au>
2026-08-29 05:49:36 +0000

    java

--- java.js
+++ java.js
@@ -1,19 +1,16 @@
-if (page1){
-    const radioButtons = document.querySelectorAll('input[name="sunny"]');
-    const radioButtons = document.querySelectorAll('input[name="rainy"]');
-    const radioButtons = document.querySelectorAll('input[name="cold"]');
+const scores = {
+  rainy: 0,
+  cold: 0,
+  sunny: 0,
+};
 
-radioButtons.forEach(radio => {
-  radio.addEventListener('change', (event) => {
-    const selectedValue = event.target.value;})
-    //let UserAnswers = localStorage.getItem('Answers', selectedValue);
-  localStorage.setItem("answers", JSON.stringify(selectedValue));
-  let result = JSON.parse(localStorage.getItem("answers")) || [];
+const buttons = document.querySelectorAll('.btn');
+const nextButton = document.getElementById('nextButton');
+const backButton = document.getElementById('backButton');
+
+nextButton.addEventListener('click', function() {
+  const input = document.querySelector('input:checked');
+  const choice = input.value; 
+  scores[choice] += 1;
+  console.log(scores);
 })
-
-  console.log(result);
-
-
-
-
-  }
