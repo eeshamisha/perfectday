4bcde07237ed8c0dd1b99ecc1a4f79479f506590
katherineqi123 <katherine.qi@education.nsw.gov.au>
2026-08-29 06:54:14 +0000

    few

--- java.js
+++ java.js
@@ -19,5 +19,4 @@ const results = document.getElementById('resultspage');
 if (results){
   const highestPlayer = scores.reduce((max, item) => item > max.item ? item : max);
   console.log(highestPlayer);
-   
 }
